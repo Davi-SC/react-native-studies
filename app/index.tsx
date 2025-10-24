f372643ecb943b0fcd468feff7ac78fd725f156e
Davi Cruz <cruzdavi001@gmail.com>
2025-10-24 00:15:30 -0300

    botao de demonstracao de barStyle e hidden

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,59 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function Index() {
+  const [dark, setDark] = useState(false);
+  const [hidden, setHidden] = useState(false);
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={[
+        styles.container,
+        {
+          backgroundColor: dark ? "#000" : "#fff",
+          paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+        },
+      ]}
     >
-      <Text></Text>
+      <StatusBar
+        barStyle={dark ? "light-content" : "dark-content"}
+        backgroundColor={dark ? "#000" : "#fff"}
+        hidden={hidden}
+      />
+
+      <View style={styles.container}>
+        <Text
+          style={{
+            color: dark ? "#fff" : "#000",
+            textAlign: "center",
+            marginBottom: 20,
+            fontSize: 20,
+          }}
+        >
+          Demonstração do comportamento do StatusBar
+        </Text>
+        <View style={{ height: 30 }} />
+        <Button title="Mudar tema" onPress={() => setDark(!dark)} />
+        <View style={{ height: 20 }} />
+        <Button
+          title={hidden ? "Mostrar StatusBar" : "Esconder StatusBar"}
+          onPress={() => setHidden(!hidden)}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
